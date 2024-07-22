1aba7c73ec55fbb45ffdcde967de047dd5b068ae
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-07-22 13:31:53 +0300

    Формы в Джанго

---
+++ HW/task5/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from task5.forms import UserRegister
+
+users = ['user1', 'user2', 'user3']
+
+
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in users:
+                info['error'] = 'Пользователь уже существует'
+            else:
+                return render(request, 'success.html', {'message': f'Приветствуем, {username}!'})
+
+    else:
+        form = UserRegister()
+    info['form'] = form
+    return render(request, 'fifth_task/registration_page.html', info)
+
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        repeat_password = request.POST['repeat_password']
+        age = int(request.POST['age'])
+
+        if password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif age < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif username in users:
+            info['error'] = 'Пользователь уже существует'
+        else:
+            return render(request, 'success.html', {'message': f'Приветствуем, {username}!'})
+
+    return render(request, 'fifth_task/registration_page.html', info)
